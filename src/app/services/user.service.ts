7ad690ea6d5141f774d24ab6631f37bfd5a076bf
eamonboyle <blaowskate@hotmail.com>
2020-02-13 14:51:45 +0000

    Add method to get the user from firebase

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,15 +3,20 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { User } from './user';
 import { USERS_CHILD } from './database-constants';
 import 'firebase/storage';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private fireDb: AngularFireDatabase) { }
+	constructor(private fireDb: AngularFireDatabase) { }
 
-  public addUser(user: User): void {
-    this.fireDb.object(`${USERS_CHILD}/${user.uid}`).set(user);
-  }
+	public addUser(user: User): void {
+		this.fireDb.object(`${USERS_CHILD}/${user.uid}`).set(user);
+	}
+
+	public getUser(uid: string): Observable<User> {
+		return this.fireDb.object<User>(`${USERS_CHILD}/${uid}`).valueChanges();
+	}
 }
