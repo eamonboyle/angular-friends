a9a38442780661a3ec3b0ddf5da955f9ab20eb2c
eamonboyle <blaowskate@hotmail.com>
2020-02-14 14:22:31 +0000

    Add friend service

---
+++ src/app/services/friends.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { Friend } from './friend';
+import { FRIENDS_CHILD, USER_DETAILS_CHILD } from './database-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendsService {
+
+  constructor(
+    private fireDb: AngularFireDatabase
+  ) { }
+
+  getFirstPage(uid: string, pageSize: number): Observable<Friend[]> {
+    return this.fireDb.list<Friend>(`${USER_DETAILS_CHILD}/${FRIENDS_CHILD}/${uid}`, ref => ref.limitToFirst(pageSize)).valueChanges();
+  }
+
+  loadNextPage(uid: string, friendKey: string, pageSize: number): Observable<Friend[]> {
+    return this.fireDb.list<Friend>(`${USER_DETAILS_CHILD}/${FRIENDS_CHILD}/${uid}`, ref => ref.orderByKey().startAt(friendKey).limitToFirst(pageSize + 1)).valueChanges();
+  }
+
+  loadPreviousPage(uid: string, friendKey: string, pageSize: number): Observable<Friend[]> {
+    return this.fireDb.list<Friend>(`${USER_DETAILS_CHILD}/${FRIENDS_CHILD}/${uid}`, ref => ref.orderByKey().startAt(friendKey).limitToLast(pageSize + 1)).valueChanges();
+  }
+}
